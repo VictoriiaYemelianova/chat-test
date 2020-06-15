fd6849243686581c72f1e8dd3c1602730d76d38a
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-06-15 15:58:57 +0300

    add admin chat

--- angular-chat/src/app/admin-message/admin-message.component.ts
+++ angular-chat/src/app/admin-message/admin-message.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-admin-message',
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-message.component.scss']
 })
 export class AdminMessageComponent implements OnInit {
+  public chatOpened: boolean;
+  public positionEl: number;
 
-  constructor() { }
+  constructor( private el: ElementRef ) {
+    this.chatOpened = false;
+  }
 
   ngOnInit(): void {
   }
 
+  openChat() {
+    if (!this.chatOpened) {
+      this.getPosition();
+    }
+    this.chatOpened = !this.chatOpened;
+  }
+
+  getPosition() {
+    const positionNativeEl = this.el.nativeElement.getBoundingClientRect();
+    const pageHeight = window.innerHeight;
+    const bottom = positionNativeEl.bottom - pageHeight;
+
+    if (positionNativeEl.top >= 300) {
+      this.positionEl = positionNativeEl.top - 300;
+    }
+
+    if (bottom > 300) {
+      this.positionEl = positionNativeEl.bottom - positionNativeEl.height;
+    }
+
+    return this.positionEl;
+  }
 }
