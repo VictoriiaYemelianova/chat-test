7a6dc73ff3748da0d6e711843099816eeb52e3e6
Victoriia Yemelianova <emelyanovavikaa@gmail.com>
2020-06-16 13:54:54 +0300

    add columns to db

---
+++ back-chat/migrations/20200616104807-roomId-column.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'Messages',
+      'roomId',
+      Sequelize.INTEGER
+
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Messages', 'roomId')
+  }
+};
